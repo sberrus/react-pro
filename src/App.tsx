cbe87bd5b0c56c8707c384889e04e114857a15a9
samuel <samuelberrus@gmail.com>
2023-05-03 21:32:28 +0200

    s2 c11

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Counter from './bases/Counter'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hola mundo desde react</h1>
+      <hr />
+      <Counter initialValue={12} />
+    </div>
+  )
+}
+
+export default App
---
+++ src/bases/Counter.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+
+interface CounterProps {
+  initialValue: number
+}
+
+const Counter = ({ initialValue = 0 }: CounterProps) => {
+  const [counter, setCounter] = useState(initialValue)
+
+  const addCount = () => {
+    setCounter((counter) => counter + 1)
+  }
+  const substractCount = () => {
+    setCounter((counter) => counter - 1)
+  }
+  const resetCount = () => {
+    setCounter(0)
+  }
+  return (
+    <div>
+      <h2>Counter {counter}</h2>
+      <button onClick={addCount}>+1</button>
+      <button onClick={resetCount}>reset</button>
+      <button onClick={substractCount}>-1</button>
+    </div>
+  )
+}
+
+export default Counter
